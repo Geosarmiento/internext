2f822b597b428faab1adf28b27fb16228850e8c0
Geosarmiento <jhova29@gmail.com>
2023-03-05 15:00:00 -0500

    pagina soporte

--- src/pages/Servicios.js
+++ src/pages/Servicios.js
@@ -2,13 +2,49 @@ import React, { Component } from "react";
 import BannerDedicado from "../Component/BannerDedicado";
 import Footer from "../Component/Footer";
 import Noticias from "../Component/Noticias";
+import iconreloj from "../assets/img/iconreloj.png";
+import fotoinstalador from "../assets/img/fotoinstalador.png";
 
-class Servicios extends Component{
-    render(){
-        return(
+class Servicios extends Component {
+    render() {
+        return (
             <div>
-           este es la pagina de servicios
-           <div>
+                 < div className="fondo">
+                <div className="container servicios">
+
+                    <div className="text_empresas">
+                        <h1>Tiempo de respuesta</h1>
+                        <h3>Atención y soporte técnico inmediato  </h3>
+                        <p>para garantizar que siempre cuentes con tu servicio de internet. </p>
+                    </div>
+                    <div className="img_servicios">
+                        <img src={iconreloj} alt="empresaslogo" />
+                    </div>
+
+
+
+
+                </div>
+
+                </div>
+
+                <div className="fondo_icon soporte1">
+                        <div className="container soporte">
+                            <img src={fotoinstalador}/> 
+                            
+                            <article>
+                                 <h1>Contamos con personal altamente capacitado</h1>  
+                                 <p>Listos para solucionar cualquier imprevisto lo más rápido posible.</p>              
+                            </article>
+                        </div>
+               
+                </div>
+
+
+
+
+
+                <div>
                     <BannerDedicado />
                 </div>
                 <div>
@@ -18,8 +54,8 @@ class Servicios extends Component{
                 <div>
                     <Footer />
                 </div>
-           </div>
-                
+            </div>
+
         );
     }
 }
