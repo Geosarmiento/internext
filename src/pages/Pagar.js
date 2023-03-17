49a49f2541fd2f284ae5e9063b55d279ccc3b054
Geosarmiento <jhova29@gmail.com>
2023-03-16 21:41:08 -0500

    page_pagar

--- src/pages/Pagar.js
+++ src/pages/Pagar.js
@@ -1,23 +1,36 @@
 import React from 'react'
+import logo_baloto from"../Styles/img/logo_baloto.png";
+import logo_efecty from"../Styles/img/logo_efecty.png";
+import logo_supergiros from"../Styles/img/logo_supergiros.png";
 
 function Pagar() {
   return (
-    <div>
+
+
+    <div className='fondo'>
+
+      <div className='container_pagar'>
         <h1>Paga tu factura de forma segura</h1>
+        <p>Selecciona tu proveedor de confianza</p>
 
-        <button><a href='https://checkout.wompi.co/l/test_sPcAnd'>Boton Epago</a></button>
+        <div className='botones_link'>
+                <button><a href='https://checkout.wompi.co/l/test_sPcAnd'>Boton Epago</a></button>
+                <button><a href='https://checkout.wompi.co/l/test_sPcAnd'>Boton WAMPI</a></button>
+        </div>
+                <div className='puntos_autorizados'>
 
-        <button><a href='https://checkout.wompi.co/l/test_sPcAnd'>Boton WAMPI</a></button>
+                  <p>Recuerda que tambien puedes pagar tu factura en los diferentes puntos autorizados</p>
 
-        <p>Recuerda que tambien puedes pagar tu factura en los diferentes puntos autorizados</p>
-
-        <p>Baloto</p>
-        <p>Efecty</p>
-        <p>SuperGiros</p>
-       
-        punto de recaudo en la direción , bario, 
-
-
+                  <div className='marcas'>
+                    <div className='logo_marcas'><img src={logo_baloto} alt="logobaloto"/></div>
+                    <div className='logo_marcas'><img src={logo_efecty} alt="logoefecty"/></div>
+                    <div className='logo_marcas'><img src={logo_supergiros} alt="logosupergiros"/></div>
+                    
+                    
+                  </div>
+                  <p>Punto de recaudo en la direción , bario,</p>
+                </div>
+      </div>
     </div>
   )
 }
