c769e129b32db8cc8ca472e6f8a499d5e3e59b37
Geosarmiento <jhova29@gmail.com>
2023-03-31 17:13:59 -0500

    logos

--- src/Component/NuestrosClientes.js
+++ src/Component/NuestrosClientes.js
@@ -1,15 +1,21 @@
 import React from 'react'
-import campo from "../Styles/img/campo.jpeg";
+import campo from "../Styles/img/campo.png";
+import audifarma from "../Styles/img/audifarma.png";
+import mobility from "../Styles/img/mobility.png";
+import soluciones from "../Styles/img/soluciones.png";
+import newwave from "../Styles/img/newwave.png";
 
 function NuestrosClientes() {
   return (
     <div className='nuestrosClientes'><h2>Nuestros Clientes</h2>
 
-        <div className='logoclientes'>
-                <img src={campo} alt='Logo1'/>
-                <img src='..' alt='Logo2'/>
-                <img src='..' alt='Logo3'/>
-                <img src='..' alt='Logo4'/>
+        <div className='logoclientes '>
+                <img src={campo} alt='campo'/>
+                <img src={audifarma} alt='audifarma'/>
+                <img src={mobility} alt='movility'/>
+                <img src={soluciones} alt='soluciones'/>
+                <img src={newwave} alt='newwave'/>
+               
         </div>
     
     
