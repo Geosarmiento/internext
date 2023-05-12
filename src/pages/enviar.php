aecbc0fbe99a165ae785c28ba5531ffdaead9245
Geosarmiento <jhova29@gmail.com>
2023-05-12 15:43:58 -0500

    enviar

--- src/pages/enviar.php
+++ src/pages/enviar.php
@@ -20,34 +20,13 @@ $message = "Mensaje: " . $_POST['message']  . " \r\n";
 $message = "Enviado el: ". date ('d/m/y' , time());
 
 $para = 'jhova29@gmail.com';
-$asunto = 'Asusnto del Mensaje';
+$asunto = 'Asunto del Mensaje';
 
 mail($para, $asunto, utf8_docode($message), $header);
 
 header("Location:index.html");
 
-
-
-/*
-error_reporting(-1);
-ini_set('display_errors', 'On');
-set_error_handler("var_dump");
-
-$subject = $_POST['subject'];// El valor entre corchetes son los atributos name del formulario html
-$msg = $_POST['msg'];
-$from = $_POST['from'];
-
-// El from DEBE corresponder a una cuenta de correo real creada en el servidor
-$headers = "From: pablomonteserin@pablomonteserin.com\r\n"; 
-$headers .= "Reply-To: $from\r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=utf-8\r\n"; 
-	
-if(mail($from, $subject, $msg,$headers)){
-	echo "mail enviado";
-	}else{
-	$errorMessage = error_get_last()['msg'];
-	echo $errorMessage;
-}
 ?>
+
+
 
